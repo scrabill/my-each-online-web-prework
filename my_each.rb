f2404934c9594bc527b32e9fb32bf18f2d726d5e
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-10 00:23:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -8,23 +8,3 @@ def my_each(array) # put argument(s) here
   end
   array
 end
-
-# def hello_t(array)
-#   if block_given?
-#     i = 0
-#
-#     while i < array.length
-#       yield(array[i])
-#       i = i + 1
-#     end
-#
-#     array
-#   else
-#     puts "Hey! No block was given!"
-#   end
-# end
-
-# collection = [1, 2, 3, 4]
-# my_each(collection) do |i|
-#   puts i
-# end
