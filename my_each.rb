860c4b3bf9bd5a196ea7526791212b5e13e1bc48
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-10 00:22:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,10 +3,25 @@ def my_each(array) # put argument(s) here
   i = 0
 
   while i < array.length
-    yield i
-    i+=
+    yield (array[i])
+    i = i + 1
   end
 end
+
+# def hello_t(array)
+#   if block_given?
+#     i = 0
+#
+#     while i < array.length
+#       yield(array[i])
+#       i = i + 1
+#     end
+#
+#     array
+#   else
+#     puts "Hey! No block was given!"
+#   end
+# end
 
 # collection = [1, 2, 3, 4]
 # my_each(collection) do |i|
